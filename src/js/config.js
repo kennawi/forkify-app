6b2d5c16e87677554fb25ff1af60bdf538347130
Ahmed Kennawi <ahmed.kennawi@kendimed.com>
2023-04-19 07:35:28 +0200

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '792d1c54-1846-4caa-9c6a-ee845505679e';
+export const MODAL_CLOSE_SEC = 2.5;
